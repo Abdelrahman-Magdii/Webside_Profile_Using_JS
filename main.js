885599c228204cbed6f0b6d341f12860808cfff5
abdelrahman <abdelrahman@abdelrahman.abdelrahman>
2023-08-02 23:40:43 +0300

    PDF CV Change

--- main.js
+++ main.js
@@ -23,6 +23,17 @@ ScrollReveal({
 ScrollReveal().reveal('.aboutimg-box');
 ScrollReveal().reveal('.details-box');
 ScrollReveal().reveal('.subHeading,  .heading, .scroll-down');
-ScrollReveal().reveal('.social-icons,.person-detail, .person-img, .service-box, .img-box, .text-box, .testi-container, .contact-container', { delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.social-icons,.person-detail, .person-img, .service-box, .img-box, .text-box, .testi-container, .contact-container', { delay: 400, origin: 'bottom' });
 
 
+// // Scrolling Animation Code 
+// ScrollReveal({
+//     reset: true,
+//     distance: '50px',
+//     duration: 1600,
+//     delay: 400
+// });
+// ScrollReveal().reveal('.navbar, .aboutimg-box', { origin: 'left' });
+// ScrollReveal().reveal('.details-box', { origin: 'right' });
+// ScrollReveal().reveal('.subHeading,  .heading, .scroll-down', { origin: 'top' });
+// ScrollReveal().reveal('.social-icons, .person-detail, .person-img, .service-box, .img-box, .text-box, .testi-container, .contact-container', { delay: 400, origin: 'bottom' });
